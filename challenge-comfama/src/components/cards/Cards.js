2d21f01b1862e7ca1269601b81800cb9d66a8607
Carlos Usuga Martinez <klich84@gmail.com>
2021-07-05 22:42:07 -0500

    04 | update files | Cards add responsive to Carousel and Styles Carousel change color box-shadown

--- challenge-comfama/src/components/cards/Cards.js
+++ challenge-comfama/src/components/cards/Cards.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Carrusel.css'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '../Card'
@@ -16,11 +16,20 @@ const useStyles = makeStyles({
 function Cards({ listAnime }) {
   const clasess = useStyles()
 
+  let initialBreackPoint = [
+    { width: 1, itemsToShow: 1},
+    { width: 550, itemsToShow: 2, itemsToScroll: 2},
+    { width: 850, itemsToShow: 5, itemsToScroll: 5},
+    { width: 1750, itemsToShow: 6, itemsToScroll: 6 },
+  ]
+
+  const [breackPoint, setBreackPoint] = useState(initialBreackPoint)
+
   return (
     <>
       <div className={clasess.root} color="primary">
 
-        <Carousel itemsToShow={5} itemsToScroll={5}>
+        <Carousel breakPoints={breackPoint}>
           {
             listAnime.map(card => (
               <Card
