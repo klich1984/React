b79088f958c6e72b5ade452c483f3f1ca8158fe8
klich1984 <klich84@gmail.com>
2022-02-21 19:04:16 -0500

    change fetch for async await

--- react-basicos/src/components/ajax/AjaxHooks.js
+++ react-basicos/src/components/ajax/AjaxHooks.js
@@ -3,30 +3,56 @@ import Pokemon from '../pokemons/Pokemon'
 
 const AjaxHooks = () => {
 	const [pokemons, setPokemons] = useState([])
+	// Peticion fetch
+	// useEffect(() => {
+	// 	let url = 'https://pokeapi.co/api/v2/pokemon'
 
+	// 	fetch(url)
+	// 		.then((res) => res.json())
+	// 		.then((json) => {
+	// 			// console.log(json)
+	// 			json.results.forEach((pokemon) => {
+	// 				fetch(pokemon.url)
+	// 					.then((res) => res.json())
+	// 					.then((json) => {
+	// 						console.log(json)
+	// 						let pokemon = {
+	// 							id: json.id,
+	// 							name: json.name,
+	// 							avatar: json.sprites.front_default,
+	// 						}
+
+	// 						// Funcion para Modificar el estado de la variable hook, y en formato de arrowFunction para actualizar el estado. Destructura lo que trae la variable pokemon + el pokemon que traes desde la peticion
+	// 						setPokemons((pokemons) => [...pokemons, pokemon])
+	// 					})
+	// 			})
+	// 		})
+	// }, [])
+
+	// Peticion async await, La funcion useEffect no se vuelve asincrono ya que se considera un antipatron (una mala practica) desde la documentación de react
 	useEffect(() => {
-		let url = 'https://pokeapi.co/api/v2/pokemon'
+		// Creo una funcion para volverla asincrona
+		const getPokemons = async (url) => {
+			let res = await fetch(url),
+				json = await res.json()
 
-		fetch(url)
-			.then((res) => res.json())
-			.then((json) => {
-				// console.log(json)
-				json.results.forEach((pokemon) => {
-					fetch(pokemon.url)
-						.then((res) => res.json())
-						.then((json) => {
-							console.log(json)
-							let pokemon = {
-								id: json.id,
-								name: json.name,
-								avatar: json.sprites.front_default,
-							}
+			// console.log(json)
+			json.results.forEach(async (el) => {
+				let res = await fetch(el.url),
+					json = await res.json()
+				//console.log(json)
+				let pokemon = {
+					id: json.id,
+					name: json.name,
+					avatar: json.sprites.front_default,
+				}
 
-							// Funcion para Modificar el estado de la variable hook, y en formato de arrowFunction para actualizar el estado. Destructura lo que trae la variable pokemon + el pokemon que traes desde la peticion
-							setPokemons((pokemons) => [...pokemons, pokemon])
-						})
-				})
+				// Funcion para Modificar el estado de la variable hook, y en formato de arrowFunction para actualizar el estado. Destructura lo que trae la variable pokemon + el pokemon que traes desde la peticion
+				setPokemons((pokemons) => [...pokemons, pokemon])
 			})
+		}
+
+		getPokemons('https://pokeapi.co/api/v2/pokemon')
 	}, [])
 
 	return (
