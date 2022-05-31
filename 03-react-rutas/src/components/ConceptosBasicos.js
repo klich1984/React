0ea4eede53a309bc92f5fbdea505a7131e47ac22
klich1984 <klich84@gmail.com>
2022-05-31 15:24:11 -0500

    Clase 63 React Router: HashRoute

--- 03-react-rutas/src/components/ConceptosBasicos.js
+++ 03-react-rutas/src/components/ConceptosBasicos.js
@@ -4,10 +4,11 @@ import {
   Routes,
   Route,
   Navigate,
+  HashRouter,
+  Link,
 } from 'react-router-dom'
 import Acerca from '../pages/Acerca'
 import Contacto from '../pages/Contacto'
-import Daschboard from '../pages/Daschboard'
 import Error404 from '../pages/Error404'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
@@ -20,6 +21,47 @@ import PrivateRoute from './PrivateRoute'
 const ConceptosBasicos = () => {
   return (
     <div>
+      <h2>Hash router</h2>
+      <HashRouter>
+        <MenuConceptos />
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/acerca">Acerca</Link>
+          <Link to="/contacto">Contacto</Link>
+        </nav>
+        <Routes>
+        <Route path='/' element={<Home />} />
+          <Route path='acerca' element={<Acerca />} />
+          <Route path='contacto' element={<Contacto />} />
+          <Route path='usuario/:username' element={<Usuario />} />
+          <Route path='productos' element={<Productos />} />
+          {/* Redirecciones */}
+          <Route
+            path='/about'
+            element={
+              <>
+                {' '}
+                <Navigate to='/acerca' />{' '}
+              </>
+            }
+          />
+          <Route
+            path='/contact'
+            element={
+              <>
+                {' '}
+                <Navigate to='/contacto' />{' '}
+              </>
+            }
+          />
+          <Route path='/react/*' element={<ReactTopics />} />
+          <Route path='/login' element={<Login />} />
+          {/* <Route path='/dashboard' element={<Daschboard />} /> */}
+          <Route path='/dashboard' element={<PrivateRoute />} />
+          <Route path='*' element={<Error404 />} />
+        </Routes>
+      </HashRouter>
+      <hr />
       <h2>Conceptos Básicos</h2>
       <Router>
         <MenuConceptos />
