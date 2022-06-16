2c272b7b2a457161ab0634576be1543842cead56
klich1984 <klich84@gmail.com>
2022-06-16 09:55:56 -0500

    Clase 88 Carrito de Compras con Reducers ( 2 / 5 )

--- 06-react-reducers/src/components/ShoppingCart.js
+++ 06-react-reducers/src/components/ShoppingCart.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useReducer } from 'react'
+import ProductItem from './ProductItem'
+import {
+  shoppingInitialState,
+  shoppingReducer,
+} from './reducers/shoppingReducer'
 
 const ShoppingCart = () => {
+  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState)
+  // console.log('State', state, dispatch)
+  const { products, cart } = state
+
+  const addToCart = (id) => {
+    console.log(id)
+  }
+
+  const delToCart = () => {}
+
+  const clearToCart = () => {}
+
   return (
     <div>
       <h2>Carrito de Compras</h2>
       <h3>Productos</h3>
-      <article className='box'></article>
+      <article className='box grid-responsive'>
+        {products.map((product) => (
+          <ProductItem key={product.id} data={product} addToCart={addToCart} />
+        ))}
+      </article>
       <h3>Carrito</h3>
       <article className='box'></article>
     </div>
