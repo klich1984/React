cabc5b6e72a0fc24e5f36bca2e6828abda45f1f3
klich1984 <klich84@gmail.com>
2022-06-17 10:00:31 -0500

    Clase 93 CRUD API con Reducers ( 2 / 2 )

--- 06-react-reducers/src/components/reducers/crudReducer.js
+++ 06-react-reducers/src/components/reducers/crudReducer.js
@@ -7,14 +7,37 @@ export const crudInitialState = {
 export function crudReducer(state, action) {
   switch (action.type) {
     case TYPES.READ_ALL_DATA: {
+      // console.log(action.payload)
+      return {
+        ...state,
+        db: action.payload.map((data) => data),
+      }
     }
     case TYPES.CREATE_DATA: {
-    }
-    case TYPES.READ_ONE_DATA: {
+      // console.log(action.payload)
+      return {
+        ...state,
+        db: [...state.db, action.payload],
+      }
     }
     case TYPES.UPDATE_DATA: {
+      // console.log(action.payload)
+      let newData = state.db.map((el) =>
+        el.id === action.payload.id ? action.payload : el
+      )
+      return {
+        ...state,
+        db: newData,
+      }
     }
     case TYPES.DELETE_DATA: {
+      // console.log(action.payload)
+      let newData = state.db.filter((el) => el.id !== action.payload)
+
+      return {
+        ...state,
+        db: newData,
+      }
     }
     case TYPES.NO_DATA: {
       return crudInitialState
