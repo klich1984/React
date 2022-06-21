1e0040e999b89c9e2a948071d50ae8493913780a
klich1984 <klich84@gmail.com>
2022-06-21 10:10:37 -0500

    Clase 94 REDUX Introducción

---
+++ 07-redux/src/components/TeoriaRedux.js
@@ -0,0 +1,52 @@
+import reduxImg from '../assets/redux.jpg'
+import reduxGif from '../assets/redux.gif'
+import '../index.css'
+
+const TeoriaRedux = () => {
+  return (
+    <div>
+      <h2>Teoría y Conceptos</h2>
+      <ul>
+        <li>
+          <b>
+            <i>Store</i>
+          </b>
+          , contiene el <i>state</i>, (uno por aplicación).
+        </li>
+        <li>
+          <b>
+            <i>Dispatch</i>
+          </b>
+          , ejecuta una acción que actualizará el <i>state</i>.
+        </li>
+        <li>
+          <b>
+            <i>Action</i>
+          </b>
+          , objeto <i>JS</i> con 2 propiedades: <i>type</i> y <i>payload</i>{' '}
+          (datos).
+        </li>
+        <li>
+          <b>
+            <i>Subscribe</i>
+          </b>
+          , es un manejador de eventos para el <i>state</i>.
+        </li>
+        <li>
+          <b>
+            <i>Reducers</i>
+          </b>
+          , son funciones que gestionan los <i>actions</i>.
+        </li>
+      </ul>
+      <div className='redux-jpg'>
+        <img src={reduxImg} alt='Diagrama Redux' />
+      </div>
+      <div className='redux-gif'>
+        <img src={reduxGif} alt='Diagrama Redux Animado' />
+      </div>
+    </div>
+  )
+}
+
+export default TeoriaRedux
