dda9d616c6b5c340450c34367256120757fc70c4
klich84 <klich84@gmail.com>
2022-08-01 15:46:05 -0500

    Uncomment react-rutas exercises

--- react-rutas/src/App.js
+++ react-rutas/src/App.js
@@ -1,29 +1,29 @@
 import logo from './logo.svg'
 import './App.css'
-// import { RuteoBasico } from './components/RuteoBasico/RuteoBasico'
+import { RuteoBasico } from './components/RuteoBasico/RuteoBasico'
 // Ejemplo de ruteo por parametrso una url que no exista
-// import RuteoPorParametros from './components/RuteoPorParametro/RuteoPorParametros'
+import RuteoPorParametros from './components/RuteoPorParametro/RuteoPorParametros'
 // Ejemplo de ruteo Anidado
-// import RuteoAnidado from './components/RuteoAnidado/RuteoAnidado'
+import RuteoAnidado from './components/RuteoAnidado/RuteoAnidado'
 // Ejemplo de Redirecciones
 import { Redirecciones } from './components/Redirecciones/Redirecciones'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Manejo de Rutas</h1>
-        <div className="App-header">
+        <div className='App-header'>
           {/* Comento para no tener los dos ejemplos corriendo al mismo tiempo */}
-          {/* <RuteoBasico /> */}
-          {/* <RuteoPorParametros /> */}
-          {/* <RuteoAnidado /> */}
+          <RuteoBasico />
+          <RuteoPorParametros />
+          <RuteoAnidado />
           <Redirecciones />
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className='App-logo' alt='logo' />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
