771aa745409a9a571fd84a546ef727c4c8d4c0bb
klich84 <klich84@gmail.com>
2022-08-17 09:22:43 -0500

    Save to localStorage functionality

--- color-picker/src/components/ColorPiker.js
+++ color-picker/src/components/ColorPiker.js
@@ -1,32 +1,71 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './ColorPicker.css'
 
 const ColorPiker = () => {
-  const [color, setColor] = useState('#fff')
+  let dataStorage = JSON.parse(localStorage.getItem('colorPicker')) || []
+  let lastColor = dataStorage[dataStorage.length - 1] || '#000000'
+
+  const [color, setColor] = useState(lastColor)
+  const [storage, setStorage] = useState(dataStorage)
 
   const handleChangeColor = (e) => {
     let value = e.target.value
     setColor(value)
   }
+  // console.log(storage)
+
+  const handleClickBtn = () => {
+    // if (storage.indexOf(color) === -1) {
+    //   let data = [...storage, color]
+    //   setStorage(data)
+    // }
+    console.log(storage.includes(color))
+    if (storage.includes(color)) return
+    let data = [...storage, color]
+    setStorage(data)
+  }
+
+  // Update LocalStorage
+  useEffect(() => {
+    localStorage.setItem('colorPicker', JSON.stringify(storage))
+  }, [storage])
 
   return (
     <div className='container-color-picker'>
       <h2>Select Color</h2>
-      <input
-        type='color'
-        name='color'
-        id='color'
-        onChange={handleChangeColor}
-      />
       <h3>Color in HEX:</h3>
       <span
-        className='colo-picker-color'
+        className='color-picker-color'
         style={{
           backgroundColor: `${color} `,
         }}
       >
         {color}
       </span>
+      <input
+        type='color'
+        name='color'
+        id='color'
+        onChange={handleChangeColor}
+        value={color}
+        // onBlur={handleBlurColor}
+      />
+      <button onClick={handleClickBtn} className='btn'>
+        Save Color
+      </button>
+      <div className='container-save-colors'>
+        {storage.map((el) => (
+          <span
+            className='color-item'
+            style={{
+              backgroundColor: `${el} `,
+            }}
+            key={el}
+          >
+            {el}
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
